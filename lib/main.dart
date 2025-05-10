ca67a078aee1b6e696bd05b96e48b96d8f917064
Bruno D'Luka <brunodlukaa@gmail.com>
2025-05-10 16:10:43 -0300

    feat: Add sentry dependency

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:totem_app/auth/controllers/auth_controller.dart';
