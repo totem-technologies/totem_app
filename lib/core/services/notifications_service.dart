cb32d459e716ad29b85f48ae2861b62f031f7490
Bruno D'Luka <brunodlukaa@gmail.com>
2025-05-13 18:01:06 -0300

    feat: Initialize local notifications

--- lib/core/services/notifications_service.dart
+++ lib/core/services/notifications_service.dart
@@ -1,15 +1,25 @@
+import 'dart:convert';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:totem_app/core/config/app_config.dart';
 import 'package:totem_app/core/errors/error_handler.dart';
 import 'package:totem_app/main.dart' show TotemApp;
 import 'package:totem_app/navigation/route_names.dart';
+
+@pragma('vm:entry-point')
+void notificationTapBackground(NotificationResponse notificationResponse) {
+  // handle action
+}
 
 class NotificationsService {
   NotificationsService._internal();
   static final NotificationsService _instance =
       NotificationsService._internal();
   static NotificationsService get instance => _instance;
+
+  final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
   bool _initialized = false;
   Future<void> initialize() async {
@@ -22,13 +32,41 @@ class NotificationsService {
       debugPrint('Initializing NotificationsService...');
       _initialized = true;
 
-      final initialMessage =
-          await FirebaseMessaging.instance.getInitialMessage();
-
-      if (initialMessage != null) {
-        _handleMessage(initialMessage);
+      // Set up Firebase
+      {
+        final initialMessage =
+            await FirebaseMessaging.instance.getInitialMessage();
+        if (initialMessage != null) {
+          _handleFirebaseMessage(initialMessage);
+        }
+        FirebaseMessaging.onMessageOpenedApp.listen(_handleFirebaseMessage);
       }
-      FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
+
+      // Set up local notifications
+      {
+        const initializationSettings = InitializationSettings(
+          android: AndroidInitializationSettings('app_icon'),
+          iOS: DarwinInitializationSettings(
+            requestAlertPermission: false,
+            requestBadgePermission: false,
+            requestSoundPermission: false,
+          ),
+        );
+        await flutterLocalNotificationsPlugin.initialize(
+          initializationSettings,
+          onDidReceiveNotificationResponse: _handleNotificationTap,
+          onDidReceiveBackgroundNotificationResponse: notificationTapBackground,
+        );
+
+        final notificationAppLaunchDetails =
+            await flutterLocalNotificationsPlugin
+                .getNotificationAppLaunchDetails();
+        if (notificationAppLaunchDetails?.notificationResponse != null) {
+          _handleNotificationTap(
+            notificationAppLaunchDetails!.notificationResponse!,
+          );
+        }
+      }
 
       debugPrint('NotificationsService initialized successfully');
     } catch (error, stackTrace) {
@@ -43,16 +81,30 @@ class NotificationsService {
     }
   }
 
-  void _handleMessage(RemoteMessage message) {
+  void _handleFirebaseMessage(RemoteMessage message) {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       debugPrint('Handling message: ${message.data}');
 
       final path = message.data['path'] as String?;
 
-      if (RouteNames.isValidRoute(path)) {
-        TotemApp.navigatorKey.currentState?.pushNamed(path!);
-      }
+      _handlePath(path);
     });
+  }
+
+  void _handleNotificationTap(NotificationResponse notificationResponse) {
+    final payload = notificationResponse.payload;
+    if (payload != null) {
+      debugPrint('Notification payload: $payload');
+      final deserializedPayload = jsonDecode(payload) as Map;
+      final path = deserializedPayload['path'] as String?;
+      _handlePath(path);
+    }
+  }
+
+  void _handlePath(String? path) {
+    if (path != null && RouteNames.isValidRoute(path)) {
+      TotemApp.navigatorKey.currentState?.pushNamed(path);
+    }
   }
 
   Future<String?> get fcmToken {
