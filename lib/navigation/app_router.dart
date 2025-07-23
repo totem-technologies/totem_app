ba724757f7a2ae17f6e9916819142ee0cbc343d4
Adil Sakout <adil.sakout@gmail.com>
2025-07-23 15:39:05 +0100

    feat: Add community guidelines screen and routing
    - Introduced CommunityGuidelinesScreen to enhance user awareness of community standards.
    - Updated app routing to include a new route for community guidelines, ensuring easy access for users.
    - Defined a new route name for community guidelines in RouteNames for better maintainability and clarity.

--- lib/navigation/app_router.dart
+++ lib/navigation/app_router.dart
@@ -5,6 +5,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:posthog_flutter/posthog_flutter.dart';
 import 'package:totem_app/auth/controllers/auth_controller.dart';
+import 'package:totem_app/auth/screens/community_guidelines.dart';
 import 'package:totem_app/auth/screens/login_screen.dart';
 import 'package:totem_app/auth/screens/onboarding_screen.dart';
 import 'package:totem_app/auth/screens/pin_entry_screen.dart';
@@ -237,7 +238,11 @@ GoRouter createRouter(WidgetRef ref) {
           return PinEntryScreen(email: email);
         },
       ),
-
+      GoRoute(
+        path: RouteNames.communityGuidelines,
+        name: RouteNames.communityGuidelines,
+        builder: (context, state) => const CommunityGuidelinesScreen(),
+      ),
       // Onboarding (no bottom nav)
       GoRoute(
         path: RouteNames.onboarding,
