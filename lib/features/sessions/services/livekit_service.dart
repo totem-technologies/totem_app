085e045129dbe330811fb696660310d548ca02d1
Bruno D'Luka <brunodlukaa@gmail.com>
2025-09-09 22:44:14 -0300

    feat: Livekit service

---
+++ lib/features/sessions/services/livekit_service.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:livekit_client/livekit_client.dart';
+import 'package:totem_app/core/config/app_config.dart';
+
+class LiveKitService {
+  LiveKitService();
+
+  Room? _room;
+  EventsListener<RoomEvent>? _listener;
+
+  Future<void> connect(String roomName, String token) async {
+    _room = Room(
+      roomOptions: const RoomOptions(
+        adaptiveStream: true,
+        dynacast: true,
+      ),
+    );
+    _listener = _room!.createListener();
+
+    await _room!.connect(
+      'wss://${AppConfig.liveKitUrl}',
+      token,
+    );
+
+    try {
+      await _room!.localParticipant!.setCameraEnabled(true);
+    } catch (error) {
+      debugPrint('Could not publish video, error: $error');
+    }
+    await _room!.localParticipant!.setMicrophoneEnabled(true);
+  }
+
+  void disconnect() {
+    _room?.disconnect();
+    _room = null;
+  }
+
+  Room? get room => _room;
+  EventsListener<RoomEvent>? get listener => _listener;
+}
