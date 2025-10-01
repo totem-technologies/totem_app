ab47450d9c49a22cf2c192bef0298eacbd8e912c
Bruno D'Luka <brunodlukaa@gmail.com>
2025-09-30 23:45:50 -0300

    feat: Add Session Title to Space Card

--- lib/shared/widgets/space_gradient_mask.dart
+++ lib/shared/widgets/space_gradient_mask.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 
 class SpaceGradientMask extends StatelessWidget {
-  const SpaceGradientMask({required this.child, super.key});
+  const SpaceGradientMask({
+    required this.child,
+    super.key,
+    this.gradientHeight = 135.0,
+  });
 
   final Widget child;
+
+  final double gradientHeight;
 
   @override
   Widget build(BuildContext context) {
     return ShaderMask(
       shaderCallback: (rect) {
         final cardHeight = rect.height;
-        const gradientHeight = 135.0;
         final startStop = ((cardHeight - gradientHeight) / cardHeight).clamp(
           0.0,
           1.0,
