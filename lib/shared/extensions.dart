96a832d8d90b84354c5ae5a6b1d1a383734627df
Bruno D'Luka <brunodlukaa@gmail.com>
2025-11-24 22:38:24 -0300

    fix: Display next sessions when the current one ends

--- lib/shared/extensions.dart
+++ lib/shared/extensions.dart
@@ -1,4 +1,7 @@
 import 'package:totem_app/api/models/event_detail_schema.dart';
+import 'package:totem_app/api/models/mobile_space_detail_schema.dart';
+import 'package:totem_app/api/models/next_event_schema.dart';
+import 'package:totem_app/api/models/public_user_schema.dart';
 
 extension ListExtension<T> on List<T> {
   List<T> reversedIf(bool condition) {
@@ -19,5 +22,35 @@ extension EventDetailSchemaExtension on EventDetailSchema {
     final now = DateTime.now();
     final end = start.add(Duration(minutes: duration));
     return start.isBefore(now.add(joinBeforeTime)) && end.isAfter(now);
+  }
+}
+
+extension MobileSpaceDetailSchemaExtension on MobileSpaceDetailSchema {
+  static MobileSpaceDetailSchema copyWith(
+    MobileSpaceDetailSchema space, {
+    String? title,
+    String? imageLink,
+    String? shortDescription,
+    String? content,
+    PublicUserSchema? author,
+    String? category,
+    int? subscribers,
+    int? price,
+    List<NextEventSchema>? nextEvents,
+    String? recurring,
+  }) {
+    return MobileSpaceDetailSchema(
+      slug: space.slug,
+      title: title ?? space.title,
+      imageLink: imageLink ?? space.imageLink,
+      shortDescription: shortDescription ?? space.shortDescription,
+      content: content ?? space.content,
+      author: author ?? space.author,
+      category: category ?? space.category,
+      subscribers: subscribers ?? space.subscribers,
+      nextEvents: nextEvents ?? space.nextEvents,
+      recurring: recurring ?? space.recurring,
+      price: price ?? space.price,
+    );
   }
 }
