c0d8021c55b67923a97d80d8a8f0e4b0c824d21b
Bruno D'Luka <brunodlukaa@gmail.com>
2026-02-04 18:35:47 -0300

    fix: Correctly invalidate home screen when session ends

--- lib/features/sessions/screens/session_ended.dart
+++ lib/features/sessions/screens/session_ended.dart
@@ -93,17 +93,27 @@ class _SessionEndedScreenState extends ConsumerState<SessionEndedScreen> {
   @override
   void initState() {
     super.initState();
-    ref.invalidate(spacesSummaryProvider);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (ref.context.mounted) ref.invalidate(spacesSummaryProvider);
+    });
+    // 2.75 seconds later, refresh spaces summary again to ensure data is up to date.
+    Future.delayed(const Duration(milliseconds: 2750), () {
+      if (ref.context.mounted) ref.invalidate(spacesSummaryProvider);
+    });
   }
 
   @override
   void dispose() {
     _confettiTimer?.cancel();
     _confettiTimer = null;
+    super.dispose();
+  }
+
+  void refresh() {
     ref
+      ..invalidate(spacesSummaryProvider)
       ..invalidate(sessionTokenProvider(widget.event.slug))
       ..invalidate(eventProvider(widget.event.slug));
-    super.dispose();
   }
 
   @override
@@ -205,12 +215,15 @@ class _SessionEndedScreenState extends ConsumerState<SessionEndedScreen> {
                           widget.event.space,
                           nextEvents: [nextEvent],
                         ),
-                        onTap: () => context.pushReplacement(
-                          RouteNames.spaceEvent(
-                            widget.event.space.slug,
-                            nextEvent.slug,
-                          ),
-                        ),
+                        onTap: () {
+                          refresh();
+                          return context.pushReplacement(
+                            RouteNames.spaceEvent(
+                              widget.event.space.slug,
+                              nextEvent.slug,
+                            ),
+                          );
+                        },
                       ),
                     ),
                   ),
@@ -232,6 +245,7 @@ class _SessionEndedScreenState extends ConsumerState<SessionEndedScreen> {
                             child: SmallSpaceCard.fromEventDetailSchema(
                               event,
                               onTap: () {
+                                refresh();
                                 return context.pushReplacement(
                                   RouteNames.space(event.space.slug),
                                 );
@@ -246,7 +260,10 @@ class _SessionEndedScreenState extends ConsumerState<SessionEndedScreen> {
                   loading: () => [],
                 ),
               ElevatedButton(
-                onPressed: () => toHome(HomeRoutes.initialRoute),
+                onPressed: () {
+                  refresh();
+                  toHome(HomeRoutes.initialRoute);
+                },
                 child: const Text('Explore More'),
               ),
             ],
