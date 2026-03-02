de3fb829d610ce952727b4d599f7619e6d695690
Adil Sakout <adil.sakout@gmail.com>
2026-03-02 16:25:44 +0000

    fix: Replace default Android notification icon with Totem icon
    Add proper white-on-transparent notification icons and configure FCM
    to use them, fixing the issue where notifications showed the default
    Android icon instead of the Totem icon.

--- lib/core/services/notifications_service.dart
+++ lib/core/services/notifications_service.dart
@@ -98,7 +98,7 @@ class NotificationsService {
       // Set up local notifications
       {
         const initializationSettings = InitializationSettings(
-          android: AndroidInitializationSettings('@mipmap/ic_launcher'),
+          android: AndroidInitializationSettings('@drawable/ic_notification'),
           iOS: DarwinInitializationSettings(
             requestAlertPermission: false,
             requestBadgePermission: false,
@@ -198,6 +198,8 @@ class NotificationsService {
         'spaces',
         'Spaces',
         channelDescription: 'Spaces',
+        icon: '@drawable/ic_notification',
+        largeIcon: DrawableResourceAndroidBitmap('@mipmap/ic_launcher'),
         importance: Importance.max,
         priority: Priority.high,
         styleInformation: DefaultStyleInformation(true, true),
