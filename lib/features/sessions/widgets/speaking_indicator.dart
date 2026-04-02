c83cae6fe24ffbf5658a00b196140df50887f62a
Bruno D'Luka <brunodlukaa@gmail.com>
2026-04-02 16:38:16 -0300

    fix: pre join screen audio indicator

--- lib/features/sessions/widgets/speaking_indicator.dart
+++ lib/features/sessions/widgets/speaking_indicator.dart
@@ -5,7 +5,7 @@ import 'package:totem_app/features/sessions/providers/emoji_reactions_provider.d
 import 'package:totem_app/features/sessions/widgets/audio_visualizer.dart';
 import 'package:totem_app/shared/totem_icons.dart';
 
-class SpeakingIndicatorAudioTrack extends StatelessWidget {
+class SpeakingIndicatorAudioTrack extends StatefulWidget {
   const SpeakingIndicatorAudioTrack({
     required this.audioTrack,
     this.participant,
@@ -21,18 +21,102 @@ class SpeakingIndicatorAudioTrack extends StatelessWidget {
   final int barCount;
 
   @override
+  State<SpeakingIndicatorAudioTrack> createState() =>
+      _SpeakingIndicatorAudioTrackState();
+}
+
+class _SpeakingIndicatorAudioTrackState
+    extends State<SpeakingIndicatorAudioTrack> {
+  EventsListener<ParticipantEvent>? _participantListener;
+  EventsListener<TrackEvent>? _trackListener;
+
+  TrackPublication<Track>? get audioTrack {
+    if (widget.participant is RemoteParticipant) {
+      return widget.participant?.getTrackPublicationBySource(
+        TrackSource.microphone,
+      );
+    } else {
+      return widget.participant?.audioTrackPublications
+          .where((t) => t.track != null && t.track!.isActive && !t.track!.muted)
+          .firstOrNull;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setup();
+  }
+
+  @override
+  void didUpdateWidget(covariant SpeakingIndicatorAudioTrack oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.participant?.sid != widget.participant?.sid) {
+      setup();
+    }
+  }
+
+  void setup() {
+    _participantListener?.dispose();
+    _participantListener = widget.participant?.createListener();
+    _participantListener
+      ?..on<TrackMutedEvent>(_onTrackMuted)
+      ..on<TrackUnmutedEvent>(_onTrackUnmuted)
+      ..on<ParticipantEvent>(_onParticipantEvent);
+
+    _trackListener?.dispose();
+    _trackListener = null;
+    if (audioTrack?.track != null) {
+      _trackListener = audioTrack!.track!.createListener();
+      _trackListener!.listen(_onTrackEvent);
+    }
+  }
+
+  void _onTrackMuted(TrackMutedEvent event) {
+    if (!mounted) return;
+    if (event.publication.source == TrackSource.microphone) setState(() {});
+  }
+
+  void _onTrackUnmuted(TrackUnmutedEvent event) {
+    if (!mounted) return;
+    if (event.publication.source == TrackSource.microphone) setState(() {});
+  }
+
+  void _onTrackEvent(TrackEvent event) {
+    if (!mounted) return;
+    setState(() {});
+  }
+
+  void _onParticipantEvent(ParticipantEvent event) {
+    if (!mounted) return;
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _participantListener?.dispose();
+    _trackListener?.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    if (audioTrack != null &&
-        !audioTrack!.muted /* && audioTrack!.subscribed */ ) {
+    if (widget.audioTrack != null &&
+        !widget.audioTrack!.muted /* && audioTrack!.subscribed */ ) {
       return LayoutBuilder(
         builder: (context, constraints) {
           return RepaintBoundary(
             child: SoundWaveformWidget(
-              audioTrack: audioTrack,
-              participant: participant,
+              audioTrack:
+                  widget.audioTrack ??
+                  (widget.participant
+                          ?.getTrackPublicationBySource(TrackSource.microphone)
+                          ?.track
+                      as AudioTrack?),
+              participant: widget.participant,
               options: AudioVisualizerWidgetOptions(
-                color: foregroundColor,
-                barCount: barCount,
+                color: widget.foregroundColor,
+                barCount: widget.barCount,
                 barMinOpacity: 0.8,
                 spacing: 2.5,
                 minHeight: constraints.maxHeight * 0.2,
@@ -46,7 +130,7 @@ class SpeakingIndicatorAudioTrack extends StatelessWidget {
       return TotemIcon(
         TotemIcons.microphoneOff,
         size: 20,
-        color: foregroundColor,
+        color: widget.foregroundColor,
       );
     }
   }
