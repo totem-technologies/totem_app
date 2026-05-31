e7d7e9d244d05ea9f139e3c1e39b92399c6d752f
Bruno D'Luka <brunodlukaa@gmail.com>
2026-05-30 21:36:05 -0300

    fix: android build

--- packages/totem_app/android/build.gradle.kts
+++ packages/totem_app/android/build.gradle.kts
@@ -11,6 +11,16 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+
+    // TODO: remove this
+    // https://github.com/prongbang/screen_protector/issues/66
+    pluginManager.withPlugin("org.jetbrains.kotlin.android") {
+        extensions.configure<org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension> {
+            compilerOptions {
+                jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+            }
+        }
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
