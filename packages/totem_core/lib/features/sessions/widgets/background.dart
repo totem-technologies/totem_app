022bd8beb50c688051cab068842a2b62c8bd0142
Bruno D'Luka <brunodlukaa@gmail.com>
2026-09-16 21:44:48 -0300

    fix: Action bar microphone foreground color

--- packages/totem_core/lib/features/sessions/widgets/background.dart
+++ packages/totem_core/lib/features/sessions/widgets/background.dart
@@ -30,6 +30,11 @@ class RoomBackground extends StatefulWidget {
   /// The [Color] passed is the background color the room is rendering.
   static ValueChanged<Color>? onBackgroundChanged;
 
+  static RoomStatus of(BuildContext context) {
+    return context.findAncestorWidgetOfExactType<RoomBackground>()?.status ??
+        RoomStatus.waitingRoom;
+  }
+
   @override
   State<RoomBackground> createState() => _RoomBackgroundState();
 }
